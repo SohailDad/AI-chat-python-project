76069649fee0b5696d1752ae044c19a1a1702a5a
SohailDAD <dsohail866@gmail.com>
2025-05-10 12:22:45 +0500

    show graphically interface of model response

--- model.py
+++ model.py
@@ -1,9 +1,19 @@
+import google.generativeai as genai
 
+
+
+# Initialize the client directly from the genai module
+genai.configure(api_key="api key ?")
+
+# Now you can access the models
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 def gemini_model(user_input):
-    client = genai.Client(api_key="api key?")
-
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", contents=f"{user_input}",
+    response = model.generate_content(
+        contents=f"{user_input}",
     )
-    print(response.text)
+    return response.text
+
+
+
+
--- streamlit.py
+++ streamlit.py
@@ -7,6 +7,7 @@ import model
 # Initialize session state for storing user inputs
 if "user_inputs" not in st.session_state:
     st.session_state.user_inputs = []
+    st.session_state.model_output = []
 
 # Input from user
 with st.form("chat_form", clear_on_submit=True):
@@ -18,14 +19,19 @@ with st.form("chat_form", clear_on_submit=True):
 
     # Add input to session state if submitted
     if submitted and user_input:
-        model.gemini_model(user_input)
+        model_output = model.gemini_model(user_input)
         st.session_state.user_inputs.append(user_input)
+        st.session_state.model_output.append(model_output)
 
 # Display all inputs dynamically
 if st.session_state.user_inputs:
     st.markdown("### Messages:")
-    st.markdown("\n".join([f"- **User:** {message}" for message in st.session_state.user_inputs]))
-
+    col1, col2 = st.columns([10, 5])
+    with col1:
+        st.markdown("\n".join([f"- **User:** {message}" for message in st.session_state.user_inputs]))
+    with col2:
+        st.markdown("\n".join([f"- **Model:** {message}" for message in st.session_state.model_output]))
+       
     
 
 
